7ba67de7f7d862e3d80087d21eedbcab0b3a73c6
emrekrdl <ek_314@hotmail.com>
2022-06-10 09:56:18 +0300

    .

---
+++ DataSet.py
@@ -0,0 +1,78 @@
+import csv
+import os
+
+import numpy as np
+from PIL import Image, ImageOps
+
+counter = [0, 0, 0, 0]
+
+
+class DataSet(object):
+
+    def __init__(self, imagepath, tagfile, image_wsize, image_hsize):
+
+        self._images = []
+        self._tags = []
+
+        with open(tagfile, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+
+            for idx, line in enumerate(reader):
+                if idx > 0:
+                    check = False
+                    if float(line[5]) == 1.0 and float(line[25]) == 1.0 and counter[0] < 2033:
+                        counter[0] += 1
+                        check = True
+                    elif float(line[5]) == 1.0 and float(line[25]) == -1.0 and counter[1] < 2033:
+                        counter[1] += 1
+                        check = True
+                    elif float(line[5]) == -1.0 and float(line[25]) == 1.0 and counter[2] < 2033:
+                        counter[2] += 1
+                        check = True
+                    elif float(line[5]) == -1.0 and float(line[25]) == -1.0 and counter[3] < 2033:
+                        counter[3] += 1
+                        check = True
+                    if check:
+                        filename = os.path.join(imagepath, line[0])
+                        im1 = Image.open(filename)
+                        im2 = ImageOps.fit(im1, size=(image_wsize, image_hsize), method=Image.LANCZOS,
+                                           centering=(0.5, 0.5)).convert('RGB')
+                        img = np.array(im2, dtype=np.uint8)
+                        self._images.append((img.astype(np.float32) / 127.5) - 1)
+                        self._tags.append([line[5], line[21], line[25]])
+                    if counter[0] == 2033 and counter[1] == 2033 and counter[2] == 2033 and counter[3] == 2033:
+                        break
+
+        self._images = np.array(self._images)
+        self._tags = np.array(self._tags).astype(np.float32)
+        self._image_num = len(self._tags)
+        self._index_in_epoch = 0
+        self.N_epoch = 0
+        rnd = np.arange(0, self._image_num)
+        np.random.shuffle(rnd)
+        self._images = self._images[rnd]
+        self._tags = self._tags[rnd]
+
+        return
+
+    def next_batch(self, batch_size=1):
+
+        read_images = []
+        caption = []
+
+        for _ in range(batch_size):
+
+            if self._index_in_epoch >= self._image_num:
+                random_idx = np.arange(0, self._image_num)
+                np.random.shuffle(random_idx)
+
+                self._images = self._images[random_idx]
+                self._tags = self._tags[random_idx]
+                self._index_in_epoch = 0
+                self.N_epoch += 1
+
+            read_images.append(self._images[self._index_in_epoch])
+            caption.append(self._tags[self._index_in_epoch])
+            self._index_in_epoch += 1
+
+        return read_images, np.array(caption)
---
+++ main.py
@@ -0,0 +1,136 @@
+import csv
+import os
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+from DataSet import DataSet
+from model import GAN
+
+image_path = './faces/'
+model_file = './models/'
+sample_path = './samples/'
+tag_file = 'list_attr_celeba.csv'
+testing_file = 'test.csv'
+
+generate_num = 1
+batch_size = 64
+image_wsize = 64
+image_hsize = 64
+oimage_wsize = 178
+oimage_hsize = 178
+caption_size = 3
+noise_size = 4096
+max_epoch = 1000
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(gpus[0], [
+            tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+    except RuntimeError as e:
+        print(e)
+
+
+def train():
+    tr = GAN(caption_size=caption_size, image_wsize=image_wsize, image_hsize=image_hsize, noise_size=noise_size)
+    checkpoint = tf.train.Checkpoint(generator=tr.generator, discriminator=tr.discriminator, g_optimizer=tr.g_optim,
+                                     d_optimizer=tr.d_optim, g_loss_meter=tr.g_loss_metrics,
+                                     d_loss_meter=tr.d_loss_metrics)
+    ckpt_manager = tf.train.CheckpointManager(checkpoint, model_file, max_to_keep=1)
+    checkpoint.restore(ckpt_manager.latest_checkpoint).expect_partial()
+    data = DataSet(imagepath=image_path, tagfile=tag_file, image_wsize=image_wsize, image_hsize=image_hsize)
+    epoch = -1
+    start = time.time()
+    while epoch < max_epoch:
+        noise = np.random.uniform(-1, 1, [batch_size, noise_size])
+        noise2 = np.random.uniform(-1, 1, [batch_size, noise_size])
+        real_images, caption = data.next_batch(batch_size=batch_size)
+        real_images = tf.reshape(real_images, shape=(batch_size, image_wsize, image_hsize, 3))
+
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as gen_tape2, tf.GradientTape() as disc_tape:
+            fake_images = tr.generator([noise, caption], training=True)
+            fake_images2 = tr.generator([noise2, caption], training=True)
+            fake_output = tr.discriminator([fake_images, caption], training=True)
+            fake_output2 = tr.discriminator([fake_images2, caption], training=False)
+            real_output = tr.discriminator([real_images, caption], training=True)
+            disc_loss = tr.d_loss_fn(real_output, fake_output)
+            gen_loss = tr.g_loss_fn(fake_output)
+            gen_loss2 = tr.g_loss_fn(fake_output2)
+
+        generator_gradients = gen_tape.gradient(gen_loss, tr.generator.trainable_variables)
+        generator_gradients2 = gen_tape2.gradient(gen_loss2, tr.generator.trainable_variables)
+        discriminator_gradients = disc_tape.gradient(disc_loss, tr.discriminator.trainable_variables)
+
+        tr.g_optim.apply_gradients(zip(generator_gradients, tr.generator.trainable_variables))
+        tr.g_optim.apply_gradients(zip(generator_gradients2, tr.generator.trainable_variables))
+        tr.d_optim.apply_gradients(zip(discriminator_gradients, tr.discriminator.trainable_variables))
+        tr.g_loss_metrics(gen_loss)
+        tr.g_loss_metrics(gen_loss2)
+        tr.d_loss_metrics(disc_loss)
+
+        if epoch != data.N_epoch:
+            epoch = data.N_epoch
+            template = '[{}/{}] D_loss={:.5f} G_loss={:.5f} time: {} sec'
+            print(template.format(epoch, max_epoch, tr.d_loss_metrics.result(),
+                                  tr.g_loss_metrics.result(), time.time() - start))
+            tr.g_loss_metrics.reset_states()
+            tr.d_loss_metrics.reset_states()
+            ckpt_manager.save()
+            start = time.time()
+    return
+
+
+def generate(rand=True):
+    tr = GAN(caption_size=caption_size, image_wsize=image_wsize, image_hsize=image_hsize, noise_size=noise_size)
+    checkpoint = tf.train.Checkpoint(generator=tr.generator, discriminator=tr.discriminator, g_optimizer=tr.g_optim,
+                                     d_optimizer=tr.d_optim, g_loss_meter=tr.g_loss_metrics,
+                                     d_loss_meter=tr.d_loss_metrics)
+    ckpt_manager = tf.train.CheckpointManager(checkpoint, model_file, max_to_keep=1)
+    checkpoint.restore(ckpt_manager.latest_checkpoint).expect_partial()
+    captions = []
+    noises = []
+    with open(testing_file, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        for idx, line in enumerate(reader):
+            noises.append(line[1:1 + noise_size])
+            captions.append(line[1 + noise_size:])
+
+    generated_images = []
+    i = 0
+    for vec, noise in zip(captions, noises):
+        i += 1
+        if i > generate_num:
+            break
+        if rand:
+            noise = np.random.uniform(-1, 1, [1, noise_size])
+            tmp = [0.0]
+            caption = np.array([[-1.0, 1.0, 1.0]]).astype(np.float32)
+            with open(testing_file, 'w') as f:
+                writer = csv.writer(f)
+                tmp = np.concatenate((tmp, noise[0]), axis=0)
+                tmp = np.concatenate((tmp, caption[0]), axis=0)
+                writer.writerow(tmp)
+        else:
+            noise = np.array([noise]).astype(np.float32)
+            caption = np.array([vec]).astype(np.float32)
+        generated_images.append(tr.generator([noise, caption]))
+
+    for i, images in enumerate(generated_images, start=1):
+        for j, image in enumerate(images, start=1):
+            img = Image.fromarray(((image.numpy() + 1) * 127.5).astype(np.uint8)).resize((oimage_wsize, oimage_hsize),
+                                                                                         Image.LANCZOS)
+            img.save(os.path.join(sample_path, 'sample_{}_{}.jpg'.format(i, j)))
+
+    return
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args[0] == '-train':
+        train()
+    elif args[0] == '-generate':
+        generate(rand=True)
