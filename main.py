2f26b06bb30017c6235d3f46877f17ced10d6480
emrekrdl <ek_314@hotmail.com>
2022-06-11 18:06:11 +0300

    .

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def train():
             for lyr in tr.discriminator.layers:
                 if type(lyr) == tf.keras.layers.GaussianNoise:
                     if lyr.stddev > 0:
-                        lyr.stddev -= 0.001
+                        lyr.stddev -= 0.0025
                     else:
                         lyr.stddev = 0
             epoch = data.N_epoch
