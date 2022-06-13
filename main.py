969ab369cb453b810c3fce371ec74daaaa57d797
emrekrdl <ek_314@hotmail.com>
2022-06-13 10:11:02 +0300

    .

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def generate(rand=True):
         reader = csv.reader(f, delimiter=',')
         for idx, line in enumerate(reader):
             noises.append(line[1:1 + noise_size])
-            captions.append(np.array([1.0, 1.0, 1.0]).astype(np.float32))
+            captions.append(np.array([-1.0, 1.0, 1.0]).astype(np.float32))
 
     generated_images = []
     i = 0
