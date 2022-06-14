734287f522d0f92b1be336073ac67a251b72d2ef
emrekrdl <ek_314@hotmail.com>
2022-06-14 16:28:12 +0300

    .

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(gpus[0], [
-            tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+            tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4608)])
     except RuntimeError as e:
         print(e)
 
